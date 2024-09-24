157a4740e79cb252d21048d13e28e450e263fea8
Karishma Gupta <karishmaneha42@gmail.com>
2024-09-24 18:29:12 +0530

    Display SleepQuality List with Emoji

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -4,8 +4,13 @@ package com.example.android.trackmysleepquality.sleeptracker
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.graphics.alpha
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.convertDurationToFormatted
+import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
+import com.example.android.trackmysleepquality.databinding.ListItemSleepNightBinding
 import com.example.android.trackmysleepquality.databinding.TextItemViewBinding
 
 
@@ -17,8 +22,9 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.SleepNightViewH
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SleepNightViewHolder {
-        val binding: TextItemViewBinding =
-            TextItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        val binding: ListItemSleepNightBinding =
+            ListItemSleepNightBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return SleepNightViewHolder(binding)
     }
 
@@ -30,16 +36,32 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.SleepNightViewH
         holder.bind(data[position])
     }
 
-    inner class SleepNightViewHolder(private val binding: TextItemViewBinding) :
+    inner class SleepNightViewHolder(private val binding: ListItemSleepNightBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(sleepNight: SleepNight) {
-            binding.tvText.text = sleepNight.sleepQuality.toString()
-            if (sleepNight.sleepQuality <= 1) {
-                binding.tvText.setTextColor(Color.RED)
-            } else {
-                binding.tvText.setTextColor(Color.BLACK)
-            }
+            fun bind(sleepNight: SleepNight) {
+                binding.sleepLength.text = sleepNight.sleepQuality.toString()
+                binding.qualityString.text = sleepNight.sleepQuality.toString()
+
+
+                val res = itemView.context.resources
+
+                binding.sleepLength.text = convertDurationToFormatted(
+                    sleepNight.startTimeMilli, sleepNight.endTimeMilli, res
+                )
+                binding.qualityString.text = convertNumericQualityToString(sleepNight.sleepQuality, res)
+
+                binding.qualityImage.setImageResource(when (sleepNight.sleepQuality) {
+                    0 -> R.drawable.ic_sleep_0
+                    1 -> R.drawable.ic_sleep_1
+                    2 -> R.drawable.ic_sleep_2
+                    3 -> R.drawable.ic_sleep_3
+                    4 -> R.drawable.ic_sleep_4
+                    5 -> R.drawable.ic_sleep_5
+                    else -> R.drawable.ic_sleep_active
+                })
         }
+
+
     }
 }
